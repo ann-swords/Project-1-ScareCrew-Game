0dbba114d313d065aa097b35802946da54c11452
Anwaar <anwaaralsyouf@gmail.com>
2022-10-02 23:26:56 +0300

    edits on buttons added

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 console.log('hi there')
 // let guessedword = 
+
+let keybored = document.querySelectorAll('.btn')
+
+
+
+//Function that prints out the letters from the keybored on screen
+keybored.forEach((element) =>{
+
+    let print = () =>{
+        console.log(element.innerHTML)
+    }
+  
+    element.addEventListener('click', print)
+}
+)
+
+
