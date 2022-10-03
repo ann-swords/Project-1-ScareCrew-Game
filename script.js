e4d6f660be5f7901be79c2c07cef8f8b355439bd
Anwaar <anwaaralsyouf@gmail.com>
2022-10-03 15:07:52 +0300

    function to show the word on screen added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Initilizing variables:
-let letter = ''
+let alphabet = ''
 let randomWords = ['Halloween', 'October', 'pumpkin', 'afraid', 'evil', 'eerie', 'gruesome', 'spooky', 'broomstick', 'witch', 'ghost', 'nightmare', 'cauldron', 'frightening', 'scary', 'darkness', 'horrify', 'disguise', 'petrify', 'terrify', 'tombstone', 'cobweb', 'cemetery', 'ghoulish', 'dead', 'haunt', 'howl', 'candy', 'superstition', 'supernatural', 'cackle', 'chilling', 'lantern', 'monster', 'moonlight', 'scream', 'grave', 'vampire', 'costume', 'flashlight', 'frightful', 'wicked', 'zombie', 'night', 'ghastly', 'creepy', 'mysterious', 'levitation']
 
 //calling the elements:
@@ -7,19 +7,29 @@ let keybored = document.querySelectorAll('.btn')
 let gussedLetterDiv = document.querySelector('.guessed-word')
 
 
-//A fucntion that generates a radnom word from the array and returns the word splitted into array of it's letters.
+//A fucntion that generates a radnom word from the array and returns the word splitted into a new array of it's letters.
 function randomWordGenerator(){
     let generatedWord = randomWords[Math.floor(Math.random() * randomWords.length)]
     console.log(generatedWord) 
 
     let seperatedWord = generatedWord.split('')
     console.log(seperatedWord)
-    
+    return seperatedWord 
 }
-randomWordGenerator()
 
 
 
+
+//Function that adds the random generated word on the screen
+function addRandomWordOnSCreen () {
+    randomWordGenerator().forEach((letter) =>{
+        let newDivLetter = document.createElement('div')
+        newDivLetter.className = 'letter-div'
+        newDivLetter.innerText = letter
+        gussedLetterDiv.appendChild(newDivLetter)
+    })
+}
+addRandomWordOnSCreen ()
 
 
 
@@ -27,12 +37,8 @@ randomWordGenerator()
 keybored.forEach((element) =>{
 
     let print = () =>{
-        letter = element.innerHTML
-        let newDivLetter = document.createElement('div')
-        newDivLetter.className = 'letter-div'
-        newDivLetter.innerHTML = letter
-        gussedLetterDiv.appendChild(newDivLetter)
-        console.log(letter)
+        alphabet = element.innerHTML
+        console.log(alphabet)
     }
   
     element.addEventListener('click', print)
