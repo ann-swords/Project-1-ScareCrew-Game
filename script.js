6f8292617446ed37b1fbdba316cfd52958545c4a
Anwaar <anwaaralsyouf@gmail.com>
2022-10-04 21:31:05 +0300

    adjustments to functions added

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let randomWords = ['Halloween', 'October', 'pumpkin', 'afraid', 'evil', 'eerie',
 
 //Generates one random word from the array and turn it to upperCase.
 let generatedWord = randomWords[Math.floor(Math.random() * randomWords.length)].toUpperCase()
-console.log(generatedWord) 
+console.log(generatedWord)
 
 //Split the word into letters and store it in an array.
 let seperatedWord = generatedWord.split('')
-console.log(seperatedWord) 
+console.log(seperatedWord)
 
 //*Calling the elements:
 //keybored --> calls the div where the buttons are placed
@@ -16,48 +16,55 @@ let keybored = document.querySelector('.keybored')
 let gussedLetterDiv = document.querySelector('.guessed-word')
 
 
-// ------------------------------------------------------------------------
+//------------------------------------------------------------------------
+
+
 //Functions:
+let wordDivs = []
+    //Function that adds 'the random' generated word on the screen
+    seperatedWord.forEach((letter) =>{
+        let newDivForLetter = document.createElement('div')
+        //first generate a new div for each letter
+        newDivForLetter.className = `${letter}`
+        newDivForLetter.style.margin = '10px'
+        newDivForLetter.style.height = '30px'
+        newDivForLetter.style.width = '30px'
+        newDivForLetter.style.borderBottom = '5px solid white'
+        gussedLetterDiv.appendChild(newDivForLetter)
+        wordDivs.push(newDivForLetter.className)
 
-//Function that adds the random generated word on the screen
-// randomWordGenerator().forEach((letter) =>{
-
-//         let newDivForLetter = document.createElement('div')
-//         //first generate a new div for each letter
-//         newDivForLetter.className = 'letter-div'
-//         gussedLetterDiv.appendChild(newDivForLetter)
-        
-//         //then generate a new paragraph to add inside the div
-//         let newLetterInsideDiv = document.createElement('p')
-//         newLetterInsideDiv.className = 'actual-letter'
-//         newLetterInsideDiv.innerText = letter
-//         newDivForLetter.appendChild(newLetterInsideDiv)
-        
-//         newLetterInsideDiv.style.display = "none"
-
-//         //if statement to compare user input to each letter
-//         //if true: 1- newLetterInsideDiv.style.display = ""
-//         //2- change the color of button to green
-//         //if false:
-//         //turn text color red
-//         //remove one part from the scarecrow
-//     })
+        console.log(wordDivs)
+        //  //then generate a new paragraph to add inside the div
+        //  let newLetterInsideDiv = document.createElement('p')
+        //  newLetterInsideDiv.className = 'actual-letter'
+        //  newLetterInsideDiv.innerText = letter
+        //  newDivForLetter.appendChild(newLetterInsideDiv)
+         
+        //  newLetterInsideDiv.style.display = "none"
+    })
 
 
-    function compare(letter){
+    function compare(letter) {
+    
+        //The letter from the user input:
         alphabet = letter.target.innerText
 
-        seperatedWord.forEach((letterInWord)=>{
-
-            if(alphabet === letterInWord){
-                console.log("comparing works!")
+        seperatedWord.forEach((letterInWord) => {
+            
+            //Compare the letter from user input to the guessd word.
+            if (alphabet === letterInWord) {
+                console.log("comparing works! letter " + alphabet + " included")
+                
             }
-
         })
-
     }
 
-   //Make the keybored clickable
-   keybored.addEventListener('click', compare)
-        
 
+//add event listen to keybored to make the letters clickable.
+keybored.addEventListener('click', compare)
+
+
+//Things to do:
+//compare user input with the exact letter position! --> stuck on that
+//if true, change alphabet color to green , then disable the button
+//if false, change alphabet color to red, then show one scarecrow part, and disable button.
